139a884b3afe9f6d88124887baa9cae179c30b73
windrichie <windrichie@gmail.com>
2019-03-19 00:16:58 +0800

    Update Module.java

--- src/main/java/seedu/address/model/person/Module.java
+++ src/main/java/seedu/address/model/person/Module.java
@@ -1,5 +1,10 @@
 package seedu.address.model.person;
 
+/**
+ * Represents a Module in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+
 public class Module {
     private TimeSlot timeslot;
 
