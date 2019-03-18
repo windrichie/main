f8b98971597b25eb1678f529a3dee865f024478a
windrichie <windrichie@gmail.com>
2019-03-19 00:52:45 +0800

    Update JsonAdaptedPerson.java

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -37,7 +37,7 @@ class JsonAdaptedPerson {
      */
     @JsonCreator
     public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
-            @JsonProperty("email") String email, @JsonProperty("address") String address, 
+            @JsonProperty("email") String email, @JsonProperty("address") String address,
             @JsonProperty("module") String module, @JsonProperty("tagged") List<JsonAdaptedTag> tagged) {
         this.name = name;
         this.phone = phone;
