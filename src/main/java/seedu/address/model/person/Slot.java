b3084bcecb31596a12c93aa10ba42e232bd93d3a
windrichie <windrichie@gmail.com>
2019-03-19 00:16:26 +0800

    fixed javadoc comment missing

--- src/main/java/seedu/address/model/person/Slot.java
+++ src/main/java/seedu/address/model/person/Slot.java
@@ -1,5 +1,10 @@
 package seedu.address.model.person;
 
+/**
+ * Represents a Slot in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+
 public class Slot {
 
     private Name name;
