8a9131a75aa932d5a1df58a065bbe9d59f5b16b1
windrichie <windrichie@gmail.com>
2019-03-19 00:15:42 +0800

    fixed javadoc comment missing

--- src/main/java/seedu/address/model/person/TimeTable.java
+++ src/main/java/seedu/address/model/person/TimeTable.java
@@ -1,5 +1,10 @@
 package seedu.address.model.person;
 
+/**
+ * Represents a Timetable in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+
 public class TimeTable {
 
 
