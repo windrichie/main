e1341907076bd1904bca99a3d417e640abf93283
Shizhe-NUS <47081861+Shizhe-NUS@users.noreply.github.com>
2019-03-18 21:38:10 +0800

    minor change

--- src/main/java/seedu/address/model/person/Module.java
+++ src/main/java/seedu/address/model/person/Module.java
@@ -1,4 +1,17 @@
 package seedu.address.model.person;
 
 public class Module {
+    private TimeSlot timeslot;
+
+
+    public TimeSlot getTimeslot(){
+        return this.timeslot;
+    }
+
+    public void setTimeSlot(TimeSlot timeslot){
+        this.timeslot = timeslot;
+    }
+
+
+
 }
--- src/main/java/seedu/address/model/person/Slot.java
+++ src/main/java/seedu/address/model/person/Slot.java
@@ -2,6 +2,7 @@ package seedu.address.model.person;
 
 public class Slot {
 
+    private Name name;
     private Image image;
     private String slotcontent;
 
