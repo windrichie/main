8cf87c4d418edbfee5691f36a3386b45d2a2b82b
Sameow <sameow.ong@gmail.com>
2019-04-01 20:40:59 +0800

    Basic structure up in place

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -16,6 +16,7 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.HistoryCommand;
+import seedu.address.logic.commands.InterleaveCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
@@ -93,6 +94,9 @@ public class AddressBookParser {
 
         case AddEventCommand.COMMAND_WORD:
             return new AddEventCommandParser().parse(arguments);
+
+        case InterleaveCommand.COMMAND_WORD:
+            return new InterleaveCommand();
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
