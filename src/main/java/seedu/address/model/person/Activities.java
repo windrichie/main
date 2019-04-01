8eb284a9484c7498d2eaadd6dfe065627251c55e
JKupoluyi <41461685+JKupoluyi@users.noreply.github.com>
2019-04-01 23:45:48 +0800

    Update Activities.java

--- src/main/java/seedu/address/model/person/Activities.java
+++ src/main/java/seedu/address/model/person/Activities.java
@@ -1,7 +1,7 @@
 package seedu.address.model.person;
 /**
  * Represents a Person's address in the address book.
- * Guarantees: immutable; is valid as declared in 
+ * Guarantees: immutable; is valid as declared in
  */
 public class Activities {
 }
