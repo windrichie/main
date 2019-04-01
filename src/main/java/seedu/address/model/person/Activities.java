45ecf5fa4c632f6f0e74af4606417ea170957b04
JKupoluyi <jok10@case.edu>
2019-04-01 23:37:41 +0800

    Made Activities class

--- src/main/java/seedu/address/model/person/Activities.java
+++ src/main/java/seedu/address/model/person/Activities.java
@@ -1,4 +1,7 @@
 package seedu.address.model.person;
-
+/**
+ * Represents a Person's address in the address book.
+ * Guarantees: immutable; is valid as declared in 
+ */
 public class Activities {
 }
