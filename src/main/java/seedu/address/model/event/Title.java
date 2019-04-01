ccc03299aad78f067d026e8c0d7f59b0617347b2
Windrich <windrichie@gmail.com>
2019-04-01 15:08:24 +0800

    Fixed Travis checkstyle error

--- src/main/java/seedu/address/model/event/Title.java
+++ src/main/java/seedu/address/model/event/Title.java
@@ -11,8 +11,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Title {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Title should only contain alphanumeric characters, punctuations and spaces, " +
-                    "and it should not be blank";
+            "Title should only contain alphanumeric characters, punctuations and spaces, "
+            + "and it should not be blank";
 
     /*
      * The first character of the title must not be a whitespace,
