427b22df16ffa7915311a67c421fa5496a8a4e4e
Sameow <sameow.ong@gmail.com>
2019-04-02 21:20:00 +0800

    preparing PPP

--- src/main/java/seedu/address/model/Interleaver.java
+++ src/main/java/seedu/address/model/Interleaver.java
@@ -1,5 +1,20 @@
 package seedu.address.model;
 
 public class Interleaver {
+    private static int FOCUS_PERIOD = 30; //in mins. Default and minimum is 30. Max is 120
+    private static int REST_DURATION = 5; //in mins. Default and minimum is 5. Max is 120
+
+    public void extractSelfStudyHours() {
+
+    }
+
+    public void split() {
+
+    }
+
+    public void interleave() {
+
+    }
+
 
 }
