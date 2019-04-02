2ecf9b9066f5223b6e0bf55c7c9360bc3053a4c7
Sameow <sameow.ong@gmail.com>
2019-04-02 21:31:21 +0800

    Fixed Travis errors
    [ant:checkstyle] [ERROR] /home/travis/build/CS2113-AY1819S2-T11-1/main/src/main/java/seedu/address/model/Interleaver.java:3: Missing a Javadoc comment. [JavadocType]

--- src/main/java/seedu/address/model/Interleaver.java
+++ src/main/java/seedu/address/model/Interleaver.java
@@ -1,5 +1,9 @@
 package seedu.address.model;
 
+
+/**
+ * All interleaving is done here
+ */
 public class Interleaver {
     private static int FOCUS_PERIOD = 30; //in mins. Default and minimum is 30. Max is 120
     private static int REST_DURATION = 5; //in mins. Default and minimum is 5. Max is 120
