3fc4dde79ac199d7c6eeafb629fc7d7a6e6a2ff6
JKupoluyi <41461685+JKupoluyi@users.noreply.github.com>
2019-04-06 02:49:25 +0800

    Update CommandBoxTest.java

--- src/test/java/seedu/address/ui/CommandBoxTest.java
+++ src/test/java/seedu/address/ui/CommandBoxTest.java
@@ -2,19 +2,16 @@ package seedu.address.ui;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
-
 import java.util.ArrayList;
 
-import javafx.scene.input.KeyCode;
-
-import seedu.address.logic.commands.CommandResult;
-
-import seedu.address.logic.commands.ListCommand;
-
-import seedu.address.logic.commands.exceptions.CommandException;
+import org.junit.Before;
+import org.junit.Test;
 
 import guitests.guihandles.CommandBoxHandle;
+import javafx.scene.input.KeyCode;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.exceptions.CommandException;
 
 public class CommandBoxTest extends GuiUnitTest {
 
