f989ee91f43b91ca2819f46f977586fda5d9b093
JKupoluyi <41461685+JKupoluyi@users.noreply.github.com>
2019-04-06 00:06:08 +0800

    Update EditCommand.java

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -26,8 +26,9 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
-import seedu.address.model.tag.Tag;
 import seedu.address.model.person.modulelist.Module;
+import seedu.address.model.tag.Tag;
+
 
 
 /**
