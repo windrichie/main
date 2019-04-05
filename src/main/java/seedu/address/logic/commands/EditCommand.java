772cfd593119566828bc84077a669d26e7a35eb4
JKupoluyi <41461685+JKupoluyi@users.noreply.github.com>
2019-04-05 23:18:51 +0800

    Update EditCommand.java

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -23,11 +23,12 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
-import seedu.address.model.person.modulelist.Module;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
+import seedu.address.model.person.modulelist.Module;
+
 
 /**
  * Edits the details of an existing person in the address book.
