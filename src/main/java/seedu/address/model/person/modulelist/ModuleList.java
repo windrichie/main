f7bfcbd511aed2eec82fe0956ccc8557b8caa8a7
JKupoluyi <41461685+JKupoluyi@users.noreply.github.com>
2019-04-06 00:25:50 +0800

    Update ModuleList.java

--- src/main/java/seedu/address/model/person/modulelist/ModuleList.java
+++ src/main/java/seedu/address/model/person/modulelist/ModuleList.java
@@ -16,7 +16,7 @@ public class ModuleList {
     public ArrayList<String> getModuleList() {
         return this.modules;
     }
-    
+
     /**
      * adds a module to the Module list of a person.
      */
