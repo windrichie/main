849c8cbebb268358dbe94787ec4c16b92446ed03
Sameow <sameow.ong@gmail.com>
2019-04-12 11:13:07 +0800

    Fix travis errors
    [ant:checkstyle] [ERROR] /home/travis/build/CS2113-AY1819S2-T11-1/main/src/main/java/seedu/address/logic/parser/ArgumentTokenizer.java:113: Line is longer than 120 characters (found 121). [LineLength]

--- src/main/java/seedu/address/logic/parser/ArgumentTokenizer.java
+++ src/main/java/seedu/address/logic/parser/ArgumentTokenizer.java
@@ -110,7 +110,8 @@ public class ArgumentTokenizer {
     }
 
     /**
-     * Returns the trimmed moduleCode of the argument in the arguments string specified by {@code currentPrefixPosition}.
+     * Returns the trimmed moduleCode of the argument in the arguments string
+     * specified by {@code currentPrefixPosition}.
      * The end position of the moduleCode is determined by {@code nextPrefixPosition}.
      */
     private static String extractArgumentValue(String argsString,
