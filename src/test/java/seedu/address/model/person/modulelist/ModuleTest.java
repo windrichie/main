2011164a096b73fbca5ab178d30c2cf39ec95693
JKupoluyi <jok10@case.edu>
2019-04-14 20:29:22 +0800

    Modulelist test

---
+++ src/test/java/seedu/address/model/person/modulelist/ModuleTest.java
@@ -0,0 +1,39 @@
+
+package seedu.address.model.person.modulelist;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import seedu.address.testutil.Assert;
+
+public class ModuleTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new Module(null));
+    }
+
+    @Test
+    public void constructor_invalidName_throwsIllegalArgumentException() {
+        String invalidModuleName = "CS32934802398";
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Module(invalidModuleName));
+    }
+
+    @Test
+    public void isValidModuleName() {
+        // null name
+        Assert.assertThrows(NullPointerException.class, () -> Module.isValidModule(null));
+
+        // invalid name
+        assertFalse(Module.isValidModule("")); // empty string
+        assertFalse(Module.isValidModule(" ")); // spaces only
+        assertFalse(Module.isValidModule("^")); // only non-alphanumeric characters
+        assertFalse(Module.isValidModule("peter*")); // contains non-alphanumeric characters
+
+        // valid name
+        assertTrue(Module.isValidModule("CS2343")); // alphabets only
+        assertTrue(Module.isValidModule("CS2143")); // numbers only
+        assertTrue(Module.isValidModule("CSS2343")); // alphanumeric characters
+        assertTrue(Module.isValidModule("AL3333")); // with capital letters
+    }
+}
